4dff6c646f9066666d14e2656a50d91d7fde99a1
Parshva Patel <parshvapatel1456@gmail.com>
2025-11-29 13:15:30 +0530

    Add files via upload

---
+++ Electronics/Testing codes/Servo_160.ino
@@ -0,0 +1,25 @@
+// for 160Ncm servo 270
+
+
+#include <Servo.h>
+
+Servo bigServo;  
+int servoPin = 9;   // PWM pin
+
+void setup() {
+  bigServo.attach(servoPin);  // attach with default 1000–2000 µs pulse
+}
+
+void loop() {
+  // Move to 0 degree position
+  bigServo.write(0);
+  delay(2000);
+
+  // Move to 135 degrees (middle)
+  bigServo.write(90);
+  delay(2000);
+
+  // Move to 270 degree position
+  bigServo.write(180);
+  delay(2000);
+}
